1dcbac4f9fd265f98a49bbfb09122c374dda863f
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-01 13:37:58 -0600

    C done for gifts

--- app/controllers/gifts.rb
+++ app/controllers/gifts.rb
@@ -7,7 +7,17 @@ MyApp.get "/gifts/new" do
 end
 
 MyApp.post "/gifts/create" do
+  @gift = Gift.new
+  @gift.title = params["title"]
+  @gift.recipient = params["recipient"]
+  @gift.description = params["description"]
+  @gift.link = params["link"]
+  @gift.cost = params["cost"]
   
+#once logins created, logged in user = @gift.user_id
+  @gift.funded = false
+  @gift.save
+  redirect "/gifts/#{@gift.id}"
 end
 
 MyApp.get "/gifts/:id" do
