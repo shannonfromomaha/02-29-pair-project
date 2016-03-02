d8067815ffda585da1f9b1c80f685afe59c884f3
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-02 16:38:16 -0600

    method to trigger funded conditions

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -7,6 +7,24 @@ class Gift < ActiveRecord::Base
     return total, remaining
   end
 
-  def activate_boolean
+  def funded_trigger
+    total, remaining = self.pledge_math
+    if total == self.cost
+      
+      self.funded = true
+
+      #to gift creator
+      @creator = User.find_by_id(self.user_id)
+      Pony.mail(:to => @creator.email, :from => 'shannonfromomaha@gmail.com', :subject => 'your thing got funded!', :body => 'yay.')
+     
+      #to pledge participants
+      @participants = Pledge.collect_pledges(self.id)
+        
+        @participants.each do |participant|
+          Pony.mail(:to => participant[1], :from => 'shannonfromomaha@gmail.com', :subject => 'gift got bought!', :body => 'yay. you did it!')
+        end
+
+    else
+    end
   end
 end
