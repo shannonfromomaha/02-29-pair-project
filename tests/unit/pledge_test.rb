5d14caecc3dcf1ac77b5d6af9fd849c7d87ae9f7
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-07 14:56:40 -0600

    cleaned up some indentation

--- tests/unit/pledge_test.rb
+++ tests/unit/pledge_test.rb
@@ -3,43 +3,44 @@ require 'test_helper'
 #setup creates users, gifts and pledges in the test database
 #test_collect_pledges tests the collect_pledges method
 class PledgeTest < Minitest::Test
-  def setup
-    super
-    @jillian = User.new
-    @jillian.name = "Jillian"
-    @jillian.email = "jillian@jillian.com"
-    @jillian.save
-
-    @shannon = User.new
-    @shannon.name = "Shannon"
-    @shannon.email = "shannon@jillian.com"
-    @shannon.save
-
-    @gift_banana= Gift.new
-    @gift_banana.save
-
-    @gift_potato = Gift.new
-    @gift_potato.save
-
-    @jillianpledge = Pledge.new
-    @jillianpledge.gift_id = @gift_banana.id
-    @jillianpledge.user_id = @jillian.id
-    @jillianpledge.amount = 5.00
-    @jillianpledge.save
-
-    @shannonpledge = Pledge.new
-    @shannonpledge.gift_id = @gift_banana.id
-    @shannonpledge.user_id = @shannon.id
-    @shannonpledge.amount = 10.00
-    @shannonpledge.save
-  end
-
-  def test_collect_pledges
-    assert_includes(Pledge.collect_pledges(@gift_banana.id),
-    ["Jillian", "jillian@jillian.com", 5.00])
-    assert_includes(Pledge.collect_pledges(@gift_banana.id),
-    ["Shannon", "shannon@jillian.com", 10.00])
-    refute_includes(Pledge.collect_pledges(@gift_potato.id),
-    ["Shannon", "shannon@jillian.com", 5.00])
-  end
+    #sets up a test database
+    def setup
+        super
+        @jillian = User.new
+        @jillian.name = "Jillian"
+        @jillian.email = "jillian@jillian.com"
+        @jillian.save
+    
+        @shannon = User.new
+        @shannon.name = "Shannon"
+        @shannon.email = "shannon@jillian.com"
+        @shannon.save
+    
+        @gift_banana= Gift.new
+        @gift_banana.save
+    
+        @gift_potato = Gift.new
+        @gift_potato.save
+    
+        @jillianpledge = Pledge.new
+        @jillianpledge.gift_id = @gift_banana.id
+        @jillianpledge.user_id = @jillian.id
+        @jillianpledge.amount = 5.00
+        @jillianpledge.save
+    
+        @shannonpledge = Pledge.new
+        @shannonpledge.gift_id = @gift_banana.id
+        @shannonpledge.user_id = @shannon.id
+        @shannonpledge.amount = 10.00
+        @shannonpledge.save
+    end
+    #tests method to find pledges for given gifts
+    def test_collect_pledges
+        assert_includes(Pledge.collect_pledges(@gift_banana.id),
+        ["Jillian", "jillian@jillian.com", 5.00])
+        assert_includes(Pledge.collect_pledges(@gift_banana.id),
+        ["Shannon", "shannon@jillian.com", 10.00])
+        refute_includes(Pledge.collect_pledges(@gift_potato.id),
+        ["Shannon", "shannon@jillian.com", 5.00])
+    end
 end
