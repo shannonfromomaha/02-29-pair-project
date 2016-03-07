ac12670eee0ac89f45439f4fe555f7cc28a40f15
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-07 14:15:47 -0600

    fixed 'em again...

--- tests/unit/gift_test.rb
+++ tests/unit/gift_test.rb
@@ -3,58 +3,58 @@ require 'test_helper'
 class GiftTest < Minitest::Test
   def setup
     super
-    @user = User.new
-    @user.name = "Jillian"
-    @user.email = "jillian@jillian.com"
-    @user.save
+    @user_a = User.new
+    @user_a.name = "Jillian"
+    @user_a.email = "jillian@jillian.com"
+    @user_a.save
 
-    @user2 = User.new
-    @user2.name = "Shannon"
-    @user2.email = "shannon@jillian.com"
-    @user2.save
+    @user_b = User.new
+    @user_b.name = "Shannon"
+    @user_b.email = "shannon@jillian.com"
+    @user_b.save
 
-    @gift = Gift.new
-    @gift.cost = 20.00
-    @gift.funded = false
-    @gift.save
+    @gift_a = Gift.new
+    @gift_a.cost = 20.00
+    @gift_a.funded = false
+    @gift_a.save
 
-    @gift2 = Gift.new
-    @gift2.cost = 100.00
-    @gift2.funded = false
-    @gift2.save
+    @gift_b = Gift.new
+    @gift_b.cost = 100.00
+    @gift_b.funded = false
+    @gift_b.save
 
-    @pledge = Pledge.new
-    @pledge.gift_id = @gift.id
-    @pledge.user_id = @user.id
-    @pledge.amount = 5.00
-    @pledge.save
+    @pledge_a = Pledge.new
+    @pledge_a.gift_id = @gift_a.id
+    @pledge_a.user_id = @user_a.id
+    @pledge_a.amount = 5.00
+    @pledge_a.save
 
-    @pledge2 = Pledge.new
-    @pledge2.gift_id = @gift.id
-    @pledge2.user_id = @user2.id
-    @pledge2.amount = 10.00
-    @pledge2.save
+    @pledge_b = Pledge.new
+    @pledge_b.gift_id = @gift_a.id
+    @pledge_b.user_id = @user_b.id
+    @pledge_b.amount = 10.00
+    @pledge_b.save
 
-    @pledge3 = Pledge.new
-    @pledge3.gift_id = @gift2.id
-    @pledge3.user_id = @user2.id
-    @pledge3.amount = 10.00
-    @pledge3.save
+    @pledge_c = Pledge.new
+    @pledge_c.gift_id = @gift_b.id
+    @pledge_c.user_id = @user_b.id
+    @pledge_c.amount = 10.00
+    @pledge_c.save
   end
 
   def test_pledge_math
-    assert_equal([15, 5], @gift.pledge_math)
-    assert_equal([10, 90], @gift2.pledge_math)
-    refute_equal([10, 5], @gift.pledge_math)
-    refute_equal([90, 10], @gift2.pledge_math)
+    assert_equal([15, 5], @gift_a.pledge_math)
+    assert_equal([10, 90], @gift_b.pledge_math)
+    refute_equal([10, 5], @gift_a.pledge_math)
+    refute_equal([90, 10], @gift_b.pledge_math)
   end
 
   def test_funded_trigger
-    @pledge4 = Pledge.new
-    @pledge4.gift_id = @gift.id
-    @pledge4.amount = 5.00
-    @pledge4.save
-    assert_equal(true, @gift.funded_trigger)
-    refute_equal(true, @gift2.funded_trigger)
+    @pledge_d = Pledge.new
+    @pledge_d.gift_id = @gift_a.id
+    @pledge_d.amount = 5.00
+    @pledge_d.save
+    assert_equal(true, @gift_a.funded_trigger)
+    refute_equal(true, @gift_b.funded_trigger)
   end
 end
