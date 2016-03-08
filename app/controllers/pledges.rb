669b8271920f1defb696b7188b4392433aaed78c
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-08 13:48:05 -0600

    test in localhost

--- app/controllers/pledges.rb
+++ app/controllers/pledges.rb
@@ -31,7 +31,7 @@ MyApp.post "/pledges/create" do
     # things happen if this new pledge funds the gift
     if @gift.funded_trigger == true
       # email to gift creator
-      User.find_by_id(@gift.user_id).funded_email_creator
+      User.find_by_id(@gift.user_id).funded_email_to_creator
       # email to pledge participants
       @participants = Pledge.collect_pledges(@gift.id)
 
--- lib/emails.rb
+++ lib/emails.rb
@@ -7,16 +7,16 @@ module Emails
   end
   #
   #here's the email we send to gift creators when their gifts are funded
-  def funded_email_creator
+  def funded_email_to_creator
     Pony.mail(:to => @self.email, :from => 'admin@kittypool.com', :subject => 'the gift you created is now funded!', :body => 'yay! some people owe you money.')
   end
   #
   #here's the email we send to pledgers when a gift is fully funded
-  def funded_email_pledges(putsomethinghere)
-    @participants = Pledge.collect_pledges(@gift.id)
-    @participants.each do |participant|
-        Pony.mail(:to => participant[1], :from => 'shannonfromomaha@gmail.c', :subject => 'gift got bought!', :body => 'yay. you did it!')
-    end
-  end
+#  def funded_email_pledges(putsomethinghere)
+#    @participants = Pledge.collect_pledges(@gift.id)
+#    @participants.each do |participant|
+#        Pony.mail(:to => participant[1], :from => 'shannonfromomaha@gmail.c', :#subject => 'gift got bought!', :body => 'yay. you did it!')
+#    end
+#  end
 #
 end
